c608353fe7ac4544768ca35e7bb42f6070d518e4
Boris Novosad <novosad.b@mail.ru>
2022-06-29 02:11:35 +0300

    Jinja Наследование расширение шаблонов

---
+++ Jinja_6.py
@@ -0,0 +1,18 @@
+# from jinja2 import Environment, FileSystemLoader
+
+# file_loader = FileSystemLoader('block')
+# env = Environment(loader=file_loader)
+#
+# template = env.get_template('about.htm')
+#
+# output = template.render()
+# print(output)
+
+# subs = ['Математика', 'Физика', 'Информатика', 'Русский']
+# file_loader = FileSystemLoader('block')
+# env = Environment(loader=file_loader)
+#
+# template = env.get_template('about.htm')
+#
+# output = template.render(list_table=subs)
+# print(output)
